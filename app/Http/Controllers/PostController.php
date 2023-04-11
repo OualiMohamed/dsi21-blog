52a241740ac3eea3539e3cfbfc204c0525896f0d
Ouali Mohamed <mohamed.ouali2010gmail.com>
2023-04-11 16:08:46 +0100

    query relationship & display Posts

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -14,7 +14,8 @@ class PostController extends Controller
      */
     public function index()
     {
-        $posts = Post::all();
+        // $posts = Post::all();
+        $posts = Post::with('category')->get();
         return view('posts.index', compact('posts'));
     }
 
