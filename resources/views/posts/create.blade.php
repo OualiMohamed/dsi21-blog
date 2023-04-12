5ac3d7447b57a092307d1b55e226482a9ea82e00
Ouali Mohamed <mohamed.ouali2010gmail.com>
2023-04-12 11:53:22 +0100

    prepare new post form

---
+++ resources/views/posts/create.blade.php
@@ -0,0 +1,43 @@
+@extends('layouts.app')
+
+@section('content')
+    <h2 class="py-4">New Post</h2>
+    <form action="{{ route('posts.store') }}" method="post">
+        @csrf
+        <div class="mb-3">
+            <label for="title" class="form-label">Title</label>
+            <input type="text" class="form-control" value="{{ old('title') }}" name="title" id="title"
+                placeholder="Enter post title">
+        </div>
+        <div class="mb-3">
+            <label for="content" class="form-label">Content</label>
+            <textarea class="form-control" name="content" id="content" rows="3" placeholder="Enter content">{{ old('content') }}</textarea>
+        </div>
+        <div class="mb-3">
+            <label for="user" class="form-label">Author</label>
+            <select class="form-select" id="user" name="user" aria-label="Default select example">
+                <option selected>Select the author</option>
+                @foreach ($authors as $author)
+                    <option value="{{ $author->id }}">{{ $author->name }}</option>    
+                @endforeach
+            </select>
+        </div>
+        <div class="mb-3">
+            <label for="category" class="form-label">Category</label>
+            <select class="form-select" id="category" name="category" aria-label="Default select example">
+                <option selected>Select a category</option>
+                @foreach ($categories as $category)
+                    <option value="{{ $category->id }}">{{ $category->name }}</option>    
+                @endforeach
+            </select>
+        </div>
+        <div class="mb-3">
+            <label for="image" class="form-label">Image</label>
+            <input class="form-control" type="file" id="image">
+        </div>
+        <div>
+            <button type="submit" class="btn btn-primary">Save</button>
+            <a href="{{ url('student-list') }}" class="btn btn-danger">Back</a>
+        </div>
+    </form>
+@endsection
